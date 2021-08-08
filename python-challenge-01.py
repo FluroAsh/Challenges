e2bc2db29c8a5b9fa8e6b76c9dad5e40d081181e
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-09 00:51:53 +1000

    Update python-challenge-01.py

--- python-challenge-01.py
+++ python-challenge-01.py
@@ -1,5 +1,5 @@
-# We assigned the mystery text to a variable we called string. 
+# Assigned the mystery text to a variable we called string
 string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-# Pass the desired string and the shifted string to the maketrans method. 
+# Passed the desired string and the shifted string to the maketrans method
 print(string.translate(str.maketrans('abcdefghijklmnopqrstuvwxyz', 'cdefghijklmnopqrstuvwxyzab')))

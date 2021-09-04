0dbc51d6f0138f2b3eeef8a7302c2f3c9ebdcf32
Ashley Thompson <orangeash@hotmail.com.au>
2021-09-04 21:10:59 +1000

    Update CA-classes-advanced.py

--- CA-classes-advanced.py
+++ CA-classes-advanced.py
@@ -23,34 +23,37 @@ class Robot:
         if self.obstacle_found:
             self.direction = (self.direction + 180) % 360
             self.obstacle_found = False
-  
-    def __add__(self, num):
-        self.speed += num 
 
-    def __sub__(self, num):
-        self.speed -= num
+    def __add__(self, value):
+        self.speed += value
 
+    def __sub__(self, value):
+        self.speed -= value
 
 
 class DriveBot(Robot):
 
     def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
         super().__init__(motor_speed, direction, sensor_range)
-    
-    def __add__(self, num):
-        super().__add__(num)
-        self.sensor_range += num
-    
-    def __sub__(self, num):
-        super().__sub__(num)
-        self.sensor_range -= num
 
-        
+    def __add__(self, value):
+        super().__add__(value)
+        self.sensor_range += value
+
+    def __sub__(self, value):
+        super().__sub__(value)
+        self.sensor_range -= value
+
+
 class WalkBot(Robot):
+    WalkBot_Count = 0
 
     def __init__(self, steps_per_minute = 0, direction = 180, sensor_range = 10, step_length = 5):
         super().__init__(steps_per_minute, direction, sensor_range)
         self.step_length = step_length
+        WalkBot.WalkBot_Count += 1
+        if Robot.robot_count >= 10 and WalkBot.WalkBot_Count >= 5: # Prevents a Robot uprising!
+            Robot.all_disabled = True
 
     def adjust_sensor(self, new_sensor_range):
        super().adjust_sensor(new_sensor_range)
@@ -67,25 +70,23 @@ class WalkBot(Robot):
            self.speed /= 2
            self.step_length /= 2
 
-    def __add__(self, num):
-        super().__add__(num)
-        self.step_length += num/2
-    
-    def __sub__(self, num):
-        super().__sub__(num)
-        self.step_length -= num/2
+    def __add__(self, value):
+       super().__add__(value)
+       self.step_length += value / 2
 
-# Initialize our Robots!
+    def __sub__(self, value):
+       super().__sub__(value)
+       self.step_length -= value / 2
+
 robot_1 = DriveBot()
 robot_2 = WalkBot()
-
-# Tests
-robot_1 + 20
-robot_1 - 10
-robot_2 + 10
-robot_2 - 5
-
-print(robot_1.speed)
-print(robot_1.sensor_range)
-print(robot_2.speed)
-print(robot_2.step_length)
+robot_3 = DriveBot()
+robot_4 = DriveBot()
+robot_5 = WalkBot()
+robot_6 = DriveBot()
+robot_7 = DriveBot()
+robot_8 = WalkBot()
+robot_9 = WalkBot()
+print(Robot.all_disabled)
+robot_10 = WalkBot()
+print(Robot.all_disabled)
